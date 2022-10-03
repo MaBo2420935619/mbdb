35210120074781cbc9db6f51e78daadba5fcd31b
mabo_9704 <mabo_9704@163.com>
2022-10-03 15:27:19 +0800

    创建sql解析，执行增删改插sql

--- src/main/java/com/ibd/database/controller/ExampleController.java
+++ src/main/java/com/ibd/database/controller/ExampleController.java
@@ -9,9 +9,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ExampleController {
+    /**
+     * 可以基于增删改查操作进行封装，实现madbClient
+     * @param args
+     */
     public static void main(String[] args) {
         SQLService service=new SQLService();
 //        service.update("update person set  name=第二,age=121321 where id=2");
-        service.inset("insert into person (id,name,age) values (1234,123,132)");
+//        service.inset("insert into person (id,name,age) values (1234,123,132)");
+//        service.delete("delete from person where id=1234");
+        service.select("select * from person where id=123");
     }
 }
